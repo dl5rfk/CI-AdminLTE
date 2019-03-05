b049a14f12a8afe3c2b00fc342e278b42b1c3b47
Klaus <dl5rfk@users.noreply.github.com>
2019-03-05 17:38:14 +0100

    Update dp_config.php

--- application/config/common/dp_config.php
+++ application/config/common/dp_config.php
@@ -7,11 +7,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 |
 */
+
 // Possible hosts locally. You can add some if needed.
-$config['host_dev'] = array('localhost', '127.0.0.1', '::1');
+$config['host_dev'] = array('localhost:8080');
+$host_port = (in_array($_SERVER['SERVER_PORT'], array('8080'), TRUE)) ? '8080' : '';
 
 // Fill in the file of your project here when you develop locally.
-$host_dev = 'CI-AdminLTE';
+$subdirectory = 'shoping';
 
 // Fill in the domain name here when your project is online.
 // Example : www.johndoe.com
@@ -19,9 +21,11 @@ $host_dev = 'CI-AdminLTE';
 $host_prod = 'your_domain.tld';
 
 // WARNING: Do not modify the lines below
-$domain = (in_array($_SERVER['HTTP_HOST'], $config['host_dev'], TRUE)) ? $_SERVER['HTTP_HOST'] . '/' . $host_dev : $host_prod;
-
+$domain = (in_array($_SERVER['HTTP_HOST'], $config['host_dev'], TRUE)) ? $_SERVER['HTTP_HOST'] . '/' . $subdirectory : $host_prod . '/' . $subdirectory;
 $config['base_url'] = ( ! empty($_SERVER['HTTPS'])) ? 'https://' . $domain : 'http://' . $domain;
+
+log_message('info','DP_CONFIG: Setting DOMAIN to: '.$domain);
+log_message('info','DP_CONFIG: Setting PORT to: '.$host_port);
 
 /*
 |--------------------------------------------------------------------------
@@ -49,3 +53,13 @@ $config['plugins_dir']    = $config['assets_dir'] . '/plugins';
 */
 $config['upload_dir']     = 'upload';
 $config['avatar_dir']     = $config['upload_dir'] . '/avatar';
+
+
+/*
+|--------------------------------------------------------------------------
+| YOUR OWN SETTINGS
+|--------------------------------------------------------------------------
+|
+*/
+$config['john']     = 'doe';
+$config['foo']     = 'bar';
